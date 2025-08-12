337afa8d7c765697450b1c3d55f4010937aafe75
maity-2003 <amantranmaity03@gmail.com>
2025-08-12 23:59:20 +0530

    Update Assignment 5.py

--- Assignment 5.py
+++ Assignment 5.py
@@ -4,13 +4,13 @@ name = input("Enter the student's name: ")
 
 if name in marks:
     student_marks = marks[name]
-    print(f"Bob"
-          f"{name}'s marks : {student_marks}")
+     print("{}'s marks : {}" .format(name,student_marks))
 else:
-    print(f"Student not found.")
+    print("Student not found.")
 
 #task2
 list=(1,2,3,4,5,6,7,8,9,10)
 print("Original list : ",list)
 print("Extract First Five Elements:", list[0:5])
+
 print("Reversed Extract Elements:", list[4 ::-1])
